eeadac0ec5878c61741520bc2a54ea6d3fa63339
Heartbeast <heartbeast.studios@gmail.com>
2025-06-30 16:40:26 -0600

    3 - Advance Expressions and Updating the Blend Positions

--- player/player.gd
+++ player/player.gd
@@ -2,9 +2,21 @@ extends CharacterBody2D
 
 const SPEED = 100.0
 
+var input_vector: = Vector2.ZERO
+
+@onready var animation_tree: AnimationTree = $AnimationTree
+
 func _physics_process(delta: float) -> void:
 	
-	var input_vector = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	input_vector = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	
+	if input_vector != Vector2.ZERO:
+		var direction_vector: = Vector2(input_vector.x, -input_vector.y)
+		update_blend_positions(direction_vector)
 	
 	velocity = input_vector * SPEED
 	move_and_slide()
+
+func update_blend_positions(direction_vector: Vector2) -> void:
+	animation_tree.set("parameters/StateMachine/MoveState/RunState/blend_position", direction_vector)
+	animation_tree.set("parameters/StateMachine/MoveState/StandState/blend_position", direction_vector)
