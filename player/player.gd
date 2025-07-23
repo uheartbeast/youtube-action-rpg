331cc5057c25d941ac3c3be9ccf31d5e6b2db13a
Heartbeast <heartbeast.studios@gmail.com>
2025-07-23 15:29:46 -0600

    3d - Refactor

--- player/player.gd
+++ player/player.gd
@@ -12,27 +12,30 @@ var last_input_vector: = Vector2.ZERO
 func _physics_process(delta: float) -> void:
 	var state = playback.get_current_node()
 	match state:
-		"MoveState":
-			input_vector = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+		"MoveState": move_state(delta)
+		"AttackState": pass
+		"RollState": roll_state(delta)
+
+func move_state(delta: float) -> void:
+	input_vector = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 			
-			if input_vector != Vector2.ZERO:
-				last_input_vector = input_vector
-				var direction_vector: = Vector2(input_vector.x, -input_vector.y)
-				update_blend_positions(direction_vector)
-			
-			if Input.is_action_just_pressed("attack"):
-				playback.travel("AttackState")
-			
-			if Input.is_action_just_pressed("roll"):
-				playback.travel("RollState")
-			
-			velocity = input_vector * SPEED
-			move_and_slide()
-		"AttackState":
-			pass
-		"RollState":
-			velocity = last_input_vector * ROLL_SPEED
-			move_and_slide()
+	if input_vector != Vector2.ZERO:
+		last_input_vector = input_vector
+		var direction_vector: = Vector2(input_vector.x, -input_vector.y)
+		update_blend_positions(direction_vector)
+	
+	if Input.is_action_just_pressed("attack"):
+		playback.travel("AttackState")
+	
+	if Input.is_action_just_pressed("roll"):
+		playback.travel("RollState")
+	
+	velocity = input_vector * SPEED
+	move_and_slide()
+
+func roll_state(delta: float) -> void:
+	velocity = last_input_vector * ROLL_SPEED
+	move_and_slide()
 
 func update_blend_positions(direction_vector: Vector2) -> void:
 	animation_tree.set("parameters/StateMachine/MoveState/RunState/blend_position", direction_vector)
