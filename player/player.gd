f5e0860f91567987be6773ff6e2304dde3516468
Heartbeast <heartbeast.studios@gmail.com>
2025-07-23 15:12:23 -0600

    3b - State Machine and Attack State

--- player/player.gd
+++ player/player.gd
@@ -5,18 +5,27 @@ const SPEED = 100.0
 var input_vector: = Vector2.ZERO
 
 @onready var animation_tree: AnimationTree = $AnimationTree
+@onready var playback = animation_tree.get("parameters/StateMachine/playback") as AnimationNodeStateMachinePlayback
 
 func _physics_process(delta: float) -> void:
-	
-	input_vector = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	
-	if input_vector != Vector2.ZERO:
-		var direction_vector: = Vector2(input_vector.x, -input_vector.y)
-		update_blend_positions(direction_vector)
-	
-	velocity = input_vector * SPEED
-	move_and_slide()
+	var state = playback.get_current_node()
+	match state:
+		"MoveState":
+			input_vector = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+			
+			if input_vector != Vector2.ZERO:
+				var direction_vector: = Vector2(input_vector.x, -input_vector.y)
+				update_blend_positions(direction_vector)
+			
+			if Input.is_action_just_pressed("attack"):
+				playback.travel("AttackState")
+			
+			velocity = input_vector * SPEED
+			move_and_slide()
+		"AttackState":
+			pass
 
 func update_blend_positions(direction_vector: Vector2) -> void:
 	animation_tree.set("parameters/StateMachine/MoveState/RunState/blend_position", direction_vector)
 	animation_tree.set("parameters/StateMachine/MoveState/StandState/blend_position", direction_vector)
+	animation_tree.set("parameters/StateMachine/AttackState/blend_position", direction_vector)
