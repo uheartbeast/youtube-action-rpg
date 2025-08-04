c2422c39ecbb4e636b85dcaa18e0b7c111f3307e
Heartbeast <heartbeast.studios@gmail.com>
2025-08-04 15:55:47 -0600

    4f - Grass Effect

--- world/grass.gd
+++ world/grass.gd
@@ -1,9 +1,14 @@
 extends Node2D
 
+const GRASS_EFFECT = preload("res://effects/grass_effect.tscn")
+
 @onready var area_2d: Area2D = $Area2D
 
 func _ready() -> void:
 	area_2d.area_entered.connect(_on_area_2d_area_entered)
 
 func _on_area_2d_area_entered(other_area_2d: Area2D) -> void:
+	var grass_effect_instance = GRASS_EFFECT.instantiate()
+	get_tree().current_scene.add_child(grass_effect_instance)
+	grass_effect_instance.global_position = global_position
 	queue_free()
