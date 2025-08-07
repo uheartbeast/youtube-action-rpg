af86932e5925a95d803529ede70e98296d7a3e51
Heartbeast <heartbeast.studios@gmail.com>
2025-08-07 12:51:19 -0600

    5d - Enemy Line of Sight

--- enemies/bat_enemy.gd
+++ enemies/bat_enemy.gd
@@ -7,6 +7,7 @@ const SPEED = 30
 @onready var sprite_2d: Sprite2D = $Sprite2D
 @onready var animation_tree: AnimationTree = $AnimationTree
 @onready var playback = animation_tree.get("parameters/StateMachine/playback") as AnimationNodeStateMachinePlayback
+@onready var ray_cast_2d: RayCast2D = $RayCast2D
 
 func _physics_process(delta: float) -> void:
 	var state = playback.get_current_node()
@@ -32,3 +33,11 @@ func is_player_in_range() -> bool:
 		if distance_to_player < range:
 			result = true
 	return result
+
+func can_see_player() -> bool:
+	if not is_player_in_range(): return false
+	var player: = get_player()
+	ray_cast_2d.target_position = player.global_position - global_position
+	var has_los_to_player: = not ray_cast_2d.is_colliding()
+	return has_los_to_player
+	
