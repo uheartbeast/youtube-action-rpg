ad79e3c8cc9262b1deb1fc34985475cc85f8b94a
Heartbeast <heartbeast.studios@gmail.com>
2025-08-27 15:00:19 -0600

    6a - Double Hit Bug

--- damage_boxes/hitbox.gd
+++ damage_boxes/hitbox.gd
@@ -3,3 +3,8 @@ class_name Hitbox extends Area2D
 @export var damage: = 1
 @export var knockback_amount: = 200
 @export var knockback_direction: Vector2
+
+var hit_targets: Array
+
+func clear_hit_targets() -> void:
+	hit_targets.clear()
